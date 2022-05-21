bcf6a309e7c5881f23a619e66409a869d4683374
Mariana-Usuga <Mariana-Usuga>
2022-05-21 02:10:57 -0500

    fix

---
+++ app.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const express = require('express');
+// const Question = require('./api/question/question.model')
+const expressConfig = require('./config/express');
+const routes = require('./routes')
+
+// const URI = process.env.DB_URI;
+const URI = "mongodb+srv://marktpul_user:OuByWjza6p97nO6x@marktcluster.ikcnf.mongodb.net/Quiz-contest?retryWrites=true&w=majority";
+// const PORT = 8080
+const PORT = process.env.PORT | 8080;
+
+const app = express();
+expressConfig(app);
+
+app.listen(PORT, () => {
+  // connect to database
+  try {
+    mongoose.connect(URI,
+     {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+    console.log(`Server running 🤖 at http://localhost:8080/`)
+  } catch (error) {
+    console.error(error);
+  }
+
+  routes(app);
+
+  console.log(`Server running 🤖 at http://localhost:${8080}/`);
+});
+
--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const mongoose = require('mongoose');
-const express = require('express');
-// const Question = require('./api/question/question.model')
-const expressConfig = require('./config/express');
-const routes = require('./routes')
+require('dotenv').config();
 
-// const URI = process.env.DB_URI;
-const URI = "mongodb+srv://marktpul_user:OuByWjza6p97nO6x@marktcluster.ikcnf.mongodb.net/Quiz-contest?retryWrites=true&w=majority";
-// const PORT = 8080
-
-const app = express();
-expressConfig(app);
-
-app.listen(8080, () => {
-  // connect to database
-  try {
-    mongoose.connect(URI,
-     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-    console.log(`Server running 🤖 at http://localhost:8080/`)
-  } catch (error) {
-    console.error(error);
-  }
-
-  routes(app);
-
-  console.log(`Server running 🤖 at http://localhost:${8080}/`);
-});
-
+module.exports = require('./app');
