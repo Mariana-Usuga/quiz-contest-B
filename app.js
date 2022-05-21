23a649b635e147790b129f2c9e29316daacb099b
Mariana-Usuga <Mariana-Usuga>
2022-05-21 02:30:54 -0500

    fix

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const expressConfig = require('./config/express');
 const routes = require('./routes')
 
-// const URI = process.env.DB_URI;
-const URI = "mongodb+srv://marktpul_user:OuByWjza6p97nO6x@marktcluster.ikcnf.mongodb.net/Quiz-contest?retryWrites=true&w=majority";
+const URI = process.env.DB_URI;
+// const URI = "mongodb+srv://marktpul_user:OuByWjza6p97nO6x@marktcluster.ikcnf.mongodb.net/Quiz-contest?retryWrites=true&w=majority";
 // const PORT = 8080
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 expressConfig(app);
@@ -21,7 +21,6 @@ app.listen(PORT, () => {
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected');
-    console.log(`Server running 🤖 at http://localhost:8080/`)
   } catch (error) {
     console.error(error);
   }
